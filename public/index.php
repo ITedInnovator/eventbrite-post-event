ef015be1a37298703aa0e899e13a3322d82bc0ec
Anthony <ag.church21@gmail.com>
2020-07-25 13:45:11 +0100

    Index PHP file HTML raw template for App page display.

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="main-menu"></nav>
+    </header>
+    <main>
+        
+    </main>
+
+    <footer>
+
+    </footer>
+    <script src="src/js/main.js" type="application/javascript"></script>
+</body>
